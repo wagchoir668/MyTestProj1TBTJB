337264c0b3b3e25fd4837aeee5702980e37893da
wagchoir <wagchoir@wagchoirdeMacBook-Pro.local>
2021-05-07 17:34:39 +0800

    Initial commit

---
+++ MyTestProj2/src/MyTest/Test1.java
@@ -0,0 +1,17 @@
+package MyTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("6655677rr");
+        List<Integer> list = new ArrayList<>();
+        list.add(9);
+        list.add(19);
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+        HashMap<Integer,Integer>map=Maps.newHashMap
+    }
+}
---
+++ src/main/webapp/Test/tess.java
@@ -0,0 +1,10 @@
+package main.webapp.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class tess {
+    public static void main(String[] args) {
+        Map<Integer,String> map = new HashMap<>();
+    }
+}
